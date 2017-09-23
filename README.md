0c983a0ee3685e7d1d969d19085f656835bb8c96
lukas kostensky <kostelu1@fit.cvut.cz>
2017-09-23 13:44:21 +0200

    Cleaning project, added new functionalities, added documentation

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# framework
+# Simple framework
 Simple framework which runs methods on objects in specified order.
+
+## Usage
+* Create object from framework.
+
+        FunctionsCollection functions = new FunctionsCollection(object);
+
+* Add methods into collection
+
+        functions.addFunction("functionName");
+
+* Run all operations in order
+
+        functions.runAll();
+
+* First method is called on given object. Other methods are called on previous returned object.
+* Method cannot have parameters.
+
+* Delete method from collection
+
+        functions.deleteFunction("functionName");
+        
+* Start operations over
+
+        functions.reinitialize();
+        functions.runAll();
--- src/main/java/kostensky/framework/FunctionsCollection.java
+++ src/main/java/kostensky/framework/FunctionsCollection.java
@@ -7,26 +7,43 @@ import java.util.logging.Level;
 
 /**
  * Created by lkost on 22.09.2017.
+ *
+ * FunctionsCollection saves methods which will be called in order they were added and first object on which the first
+ * method will be called.
  */
 public class FunctionsCollection {
     ArrayList<String> functionsList =  new ArrayList<String>();
     Object o;
+    Object firstObject;
 
     private final static Logger LOGGER = Logger.getLogger(FunctionsCollection.class.getName());
 
+    /*
+     * First method will be called on the object given in constructor.
+     */
     public FunctionsCollection(Object o) {
         this.o = o;
+        this.firstObject = o;
     }
 
+    /*
+     * add new method to collection.
+     */
     public void addFunction(String func) {
         functionsList.add(func);
     }
 
+    /*
+     * Runs specified method on specified object.
+     */
     public Object runNext(String functionName, Object o) throws Exception {
         Method func = o.getClass().getMethod(functionName);
         return func.invoke(o);
     }
 
+    /*
+     * Method will start operations in order they were added into collection.
+     */
     public Object runAll() throws Exception {
         for (String func : functionsList){
             o = runNext(func, o);
@@ -34,5 +51,19 @@ public class FunctionsCollection {
             LOGGER.info("Function " + func + " was called and returned " + o);
         }
         return o;
+    }
+
+    /*
+     * Deletes function from collection.
+     */
+    public boolean deleteFunction(String func){
+        return functionsList.remove(func);
+    }
+
+    /*
+     * Reinitialize collection, so it can be called again.
+     */
+    public void reinitialize(){
+        o = firstObject;
     }
 }
